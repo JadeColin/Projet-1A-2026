3324142e277e46c8f3b74cf59411001ad3248367
mathiasdouillard0 <mathias.douillard@eleve.ensai.fr>
2026-03-11 14:16:13 +0000

    lint

--- Projet_Mathias/Texte.py
+++ Projet_Mathias/Texte.py
@@ -1,4 +1,4 @@
-'''
+"""
 Si on veut intégrer des base de données trop grosse, il ne faut pas les versionner.
 
 --------------------------------------
@@ -6,4 +6,9 @@ Test :
     - Soit on lance la commande "python -m pytest" depuis le terminal
     - Soit on ouvre l'onglet Testing à gauche, ça ressemble à une fiole. Une fois dessus, il faut
     appuyer sur run test de work
-'''
+
+Lint : les peluches qui se coincent dans les vêtements. Sur VS Code, cela permet de reprérer les
+imperfections de code et parfois de les corriger automatiquement.
+Pour que la correction se fasse avec l'extension black formatter,
+il faut faire ctrl + s.
+"""
