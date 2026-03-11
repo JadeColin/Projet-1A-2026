bb777e344d373275756774ad89bcda33b7d35fb4
mathiasdouillard0 <mathias.douillard@eleve.ensai.fr>
2026-03-11 13:41:38 +0000

    jsp

---
+++ Projet_Mathias/Texte.py
@@ -0,0 +1,9 @@
+'''
+Si on veut intégrer des base de données trop grosse, il ne faut pas les versionner.
+
+--------------------------------------
+Test :
+    - Soit on lance la commande "python -m pytest" depuis le terminal
+    - Soit on ouvre l'onglet Testing à gauche, ça ressemble à une fiole. Une fois dessus, il faut
+    appuyer sur run test de work
+'''
--- data/Texte.py
+++
@@ -1,3 +0,0 @@
-'''
-Si on veut intégrer des base de données trop grosse, il ne faut pas les versionner.
-'''
