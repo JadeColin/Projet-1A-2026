16ebb4770d511d7d2192f71a9cb3d4a16820454d
lakhrissii1 <mohammed-adam.lakhrissi@eleve.ensai.fr>
2026-04-02 14:45:34 +0000

    gg

--- Projet_Adam/Match.py
+++ Projet_Adam/Match.py
@@ -1,10 +1,21 @@
+from datetime import date
+
 class Match:
-    def __init__(self, id_match: int, date_match: str, localisation: str, id_evenements: int):
+    def __init__(self, id_match: int, date_match: date, localisation: str, id_evenement: int):
         self.id_match = id_match
         self.date_match = date_match
         self.localisation = localisation
-        self.id_evenements = id_evenements  
+        self.id_evenement = id_evenement
+        self.participations = []
+        self.resultats = []
+
+    def ajouter_resultat(self, resultat):
+        self.resultats.append(resultat)
+
+    def ajouter_participation(self, participation):
+        self.participations.append(participation)
+    
 
     def __repr__(self):
         return (f"Match(id={self.id_match}, date='{self.date_match}', "
-                f"localisation='{self.localisation}', id_evenements={self.id_evenements})")
+                f"localisation='{self.localisation}', id_evenement={self.id_evenement})")
