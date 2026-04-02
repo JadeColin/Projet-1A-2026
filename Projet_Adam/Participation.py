cd34b0bd452b75b629e84d40b7a9e1cdbeda2787
lakhrissii1 <mohammed-adam.lakhrissi@eleve.ensai.fr>
2026-04-02 14:55:52 +0000

    gg

--- Projet_Adam/Participation.py
+++ Projet_Adam/Participation.py
@@ -2,7 +2,19 @@ class Participation:
     def __init__(self, id_participation: int, id_match: int, status: str):
         self.id_participation = id_participation
         self.id_match = id_match  
-        self.status = status  
+        self.status = status
+        self.resultats = []
+        self.joueurs = []
+        self.equipes = []
+
+    def ajouter_joueur(self, joueur):
+        self.joueurs.append(joueur)
+
+    def ajouter_equipe(self, equipe):
+        self.equipes.append(equipe)
+
+    def ajouter_resultat(self, resultat):
+        self.resultats.append(resultat)
 
     def __repr__(self):
         return (f"Participation(id={self.id_participation}, "
