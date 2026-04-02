d4c0a37704027d301e045fb64a80274f0dbf2dd4
fouad <mohamed-fouad.chaabane@eleve.ensai.fr>
2026-04-02 14:25:40 +0000

    bvfj

--- Projet_fouad/interface.py
+++ Projet_fouad/interface.py
@@ -1,8 +1,10 @@
-from Projet_Adam.competition import Competition
-from Projet_Adam.evenement import Evenenment
-from Projet_Mathias import 
+from src.Model.Sport import Sport
+from src.Analysis import display_all_competitions
 
-supported_sports = display_sport(comp)
+supported_sports = [
+    Sport(name="football",team_sport=True),
+    Sport(name="tennis",team_sport=False)
+]
 
 selected_sport = input("Sélectionnez un sport parmi {supported_sports}")
 if selected_sport not in supported_sports:
